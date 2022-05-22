3b634f289b2ef4c174053c6e9155d684606ad8d0
khalil hassayoun <70943561+khalil-hassayoun@users.noreply.github.com>
2022-05-22 04:21:29 +0100

    " "

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -25,3 +25,15 @@ console.log(teacher3);
 // lastName: "Doe"
 // location: "London"
 
+interface Directors extends Teacher {
+  numberOfReports: number,
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
